2daf28fbf6b5c9fa401fd7e5eaeee96c4cd25a3b
nikoleta-dobrevska <nikoletadobrevska@gmail.com>
2023-05-18 18:43:16 +0300

    Add calculating functions

--- javascript.js
+++ javascript.js
@@ -1,18 +1,34 @@
+let priceWithoutVat = 0;
+let totalPrice = 0;
+const values = document.getElementsByTagName("input");
+
+// resets inputs' values after reload or going back 
+function checkReload() {
+    let data = window.performance.getEntriesByType("navigation")[0].type;
+
+    if (data === "reload") {
+
+       for (let i = 0; i < values.length; i++) {
+        values[i].value = '';
+       }
+
+        priceWithoutVat = 0;
+        totalPrice = 0;
+    }
+}
+
+checkReload();
+
 const addButton = document.getElementsByClassName("add-button")[0];
 addButton.addEventListener('click', addItem);
 
 const main = document.getElementsByTagName("main")[0];
 const labels = ["Item", "Quantity", "Metric unit", "Price"];
 
-const quantityInputs = new Array();
-const priceInputs = new Array();
-
 function addItem() {
     const newRow = document.createElement("div");
     newRow.classList.add("row");
     main.appendChild(newRow);
-    
-    let i = 0;
 
     labels.forEach(element => {
         const newLabelHolder = document.createElement("div");
@@ -31,46 +47,63 @@ function addItem() {
         newRow.appendChild(newLabelHolder);
         newRow.appendChild(newInputHolder);
 
-        if(i==0 || i==2) {
-            newInput.id = "item";
-            newInput.type="text";
-        } else if(i==1) {
-            newInput.id = "quantity";
-            newInput.type="number"
-            newInput.step="0.001"
-            quantityInputs.push(newInput);
-        } else {
-            newInput.id = "price"
-            newInput.type="number"
-            newInput.step="0.01"
-            priceInputs.push(newInput);
+        if (element === "Quantity") {
+            newInput.setAttribute("type", "number");
+            newInput.setAttribute("step", ".001");
+            newInput.classList.add("quantity");
         }
 
-        i++;
-    });
+        if (element === "Price") {
+            newInput.setAttribute("type", "number");
+            newInput.setAttribute("step", ".01");
+            newInput.classList.add("price");
+        }
+    }); 
 
     const buttonHolder = document.createElement("div");
     buttonHolder.classList.add("add-and-remove-container");
-
-    const addButtonIcon = document.createElement("i");
-    addButtonIcon.classList.add("fa");
-    addButtonIcon.classList.add("fa-plus");
 
     const removeButtonIcon = document.createElement("i");
     removeButtonIcon.classList.add("fa");
     removeButtonIcon.classList.add("fa-minus");
 
-    const newAddButton = document.createElement("button");
-    newAddButton.classList.add("add-button");
-    newAddButton.addEventListener('click', addItem);
-    newAddButton.appendChild(addButtonIcon);
-
     const newRemoveButton = document.createElement("button");
     newRemoveButton.classList.add("remove-button");
     newRemoveButton.appendChild(removeButtonIcon);
 
-    buttonHolder.appendChild(newAddButton);
+    buttonHolder.appendChild(addButton);
     buttonHolder.appendChild(newRemoveButton);
     main.appendChild(buttonHolder);
 }
 
+const calculateWithoutVatButton = document.getElementsByClassName("calculate")[0];
+calculateWithoutVatButton.addEventListener('click', calculatePriceWithoutVat);
+
+const priceWithoutVatText = document.getElementsByTagName("span")[0];
+
+function calculatePriceWithoutVat() {
+    var prices = document.getElementsByClassName("price");
+    var quantities = document.getElementsByClassName("quantity");
+
+    for(let i = 0; i < prices.length; i++) {
+           let currentQuantity = parseFloat(prices[i].value);
+           let currentPrice = parseFloat(quantities[i].value);
+
+            priceWithoutVat += currentQuantity * currentPrice;
+    }
+
+    priceWithoutVatText.innerHTML = priceWithoutVat;
+}
+
+const calculateWithVatButton = document.getElementsByClassName("calculate")[1];
+calculateWithVatButton.addEventListener('click', calculateTotalPrice);
+
+let totalPriceText = document.getElementsByTagName("span")[1];
+
+function calculateTotalPrice() {
+    const vat = document.getElementById("vat").value;
+
+    totalPrice = (priceWithoutVat * vat) / 100 + priceWithoutVat;
+
+    totalPriceText.innerHTML = totalPrice;
+}
